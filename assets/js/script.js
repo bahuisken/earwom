fdc5b7bfb28bdb0f36f44648eb8f0132c2752cc1
Brandon Laptop <brandon.minard@gmail.com>
2021-02-06 13:49:29 -0700

    something, I'm sure

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,32 @@
 //
 
 
-document.getElementById("submit-query-btn").addEventListener("click", function () {
-
-    var searchBarValue = document.querySelector("#query").value.split(" ").join("%20");
+document.getElementById("submit-query-btn").addEventListener("click", function (event) {
+    event.preventDefault()
+    // var searchBarValue = document.querySelector("#query").value.split(" ").join("%20");
+    var searchBarValue = "rap%20god"
     // need func here to like check checkboxes and such
     // so I think we go one by one, and once one is true, we do it.
-    var queryType = figureWhatTypeQuery()
+    // var queryType = figureWhatTypeQuery()
+    var queryType = "album"
     console.log(queryType)
 
     if (queryType === "lyrics") {
+        //
+        var bestLyricResponse;
         console.log("https://api.genius.com/search?q=" + searchBarValue + "&access_token=fbzexr2DEleMzVPAdhBCCTEWXTXpMvS1pn8AmhXYmnTg0KwJxnSheU_fl3pDgUJJ")
         fetch("https://api.genius.com/search?q=" + searchBarValue + "&access_token=fbzexr2DEleMzVPAdhBCCTEWXTXpMvS1pn8AmhXYmnTg0KwJxnSheU_fl3pDgUJJ"
         )
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data.response.hits[0].result);
+                bestLyricResponse = data.response.hits[0].result
+            });
+
+
     } else {
+        //token out of date, will only work for tracks/songs.
+        //Working out a solution.
         fetch(constructSpotifyQuery(searchBarValue, queryType), {
             method: "GET",
             headers: {
@@ -37,6 +48,11 @@ document.getElementById("submit-query-btn").addEventListener("click", function (
     }
 
 });
+
+function renderLyrics(bestLyricResponse, searchQuery) {
+    //do something to render lyrics here, and save the search query.
+
+}
 
 function constructSpotifyQuery(query, type) {
     return "https://api.spotify.com/v1/search?q=" + query + "&type=" + type
