4a0edc4a413372cd31a6e323f968e55e8c4016ef
B.A. Huisken <brice.huisken@gmail.com>
2021-02-08 20:06:07 -0700

    changed tile colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,79 @@
 //https://api.genius.com/search?q="WORDS HERE"&access_token=fbzexr2DEleMzVPAdhBCCTEWXTXpMvS1pn8AmhXYmnTg0KwJxnSheU_fl3pDgUJJ
 //looking for response.response.hits[0]?
 
-//console.log(document.getElementById("submitQueryBtn"))
+
+//hopefully when that closes it redirects us back...
 
 
+//stole this -B
+// Get the hash of the url
+const hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+        if (item) {
+            var parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+    }, {});
+window.location.hash = '';
 
-//
+// Set token
+let _token = hash.access_token;
 
+const authEndpoint = 'https://accounts.spotify.com/authorize';
 
-document.getElementById("submit-query-btn").addEventListener("click", function () {
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = '484101cfe3334822a8460d3399e625f0';
+const redirectUri = 'https://bahuisken.github.io/project-1/';
+// const scopes = [
+//   'user-top-read'
+// ];
 
-    var searchBarValue = document.querySelector("#query").value.split(" ").join("%20");
+// If there is no token, redirect to Spotify authorization
+// if (!_token) {
+//     window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+// }
+//&scope=${scopes.join('%20')}
+
+document.getElementById("authBtn").addEventListener("click", function (event) {
+    if (!_token) {
+        window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+    } else {
+        alert("Already have auth token")
+    }
+})
+
+console.log(_token)
+
+document.getElementById("submit-query-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.removeClass("is-active");
+    // var searchBarValue = document.querySelector("#query").value.split(" ").join("%20");
+    var searchBarValue = "rap%20god"
     // need func here to like check checkboxes and such
     // so I think we go one by one, and once one is true, we do it.
-    var queryType = figureWhatTypeQuery()
+    // var queryType = figureWhatTypeQuery()
+    var queryType = "album"
     console.log(queryType)
 
     if (queryType === "lyrics") {
+        //
+        var bestLyricResponse;
         console.log("https://api.genius.com/search?q=" + searchBarValue + "&access_token=fbzexr2DEleMzVPAdhBCCTEWXTXpMvS1pn8AmhXYmnTg0KwJxnSheU_fl3pDgUJJ")
         fetch("https://api.genius.com/search?q=" + searchBarValue + "&access_token=fbzexr2DEleMzVPAdhBCCTEWXTXpMvS1pn8AmhXYmnTg0KwJxnSheU_fl3pDgUJJ"
         )
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data.response.hits[0].result);
+                bestLyricResponse = data.response.hits[0].result
+            });
+
+
     } else {
+        //token out of date, will only work for tracks/songs.
+        //Working out a solution.
         fetch(constructSpotifyQuery(searchBarValue, queryType), {
             method: "GET",
             headers: {
@@ -37,6 +88,11 @@ document.getElementById("submit-query-btn").addEventListener("click", function (
     }
 
 });
+
+function renderLyrics(bestLyricResponse, searchQuery) {
+    //do something to render lyrics here, and save the search query.
+
+}
 
 function constructSpotifyQuery(query, type) {
     return "https://api.spotify.com/v1/search?q=" + query + "&type=" + type
@@ -71,15 +127,15 @@ function figureWhatTypeQuery() {
 function generateResults(data) {
 
 }
-var close = $(".modal-close");
+var closeM = $(".modal-close");
 var modal = $(".modal");
-close.click(function closeModal() {
+closeM.click(function closeModal() {
 
     modal.removeClass("is-active");
 })
 
 
-var active =$("#active");
+var active = $("#active");
 active.click(function openModal() {
     modal.addClass("is-active");
 })
